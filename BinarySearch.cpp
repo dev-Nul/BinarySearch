c8c185c09dd70444eb8ccbdf1d57a76652c90f81
Zachary Morritt <zachary101332@gmail.com>
2021-02-24 16:48:40 -0800

    Added welcome greeting.

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main()
 {
-    cout<< "Hello World!\n";
+    cout << "BINARY SEARCH\n";
+    cout << "by Zachary Morritt\n\n";
+
+    cout << "This program is meant to take a chosen amount of numbers as an input, then sort them in either ascending or descending order.\n";
+    cout << "The only accepted inputs will be numbers.  If an incorrect input is entered, you will be ask to re-enter new values.\n";
+    cout << "If you wish, you can also choose a .txt file with numbers on each line, which will also be sorted.\n";
+    cout << "If you have any questions, don't hesitate to ask me for support.\n\n";
+
+    cout << "PRESS ANY KEY TO CONTINUE...\n";
+    cin.ignore();
 }
