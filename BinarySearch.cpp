a05ca19b9e2aa2a478ad98ba5d8857fa698f57c3
Zachary Morritt <zachary101332@gmail.com>
2021-02-26 01:57:45 -0800

    Added user input functionality, and patched some bugs in various functions. Missing: Formulating program flow. Note: All functionality is added as far as I am aware - all that needs to be done is putting everything together!

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,8 +5,34 @@
 #include <list>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
+
+// A function for formatting an array of integers into a string.
+// Given the array, and its length.
+string formatArray(int arr[], int arrLength)
+{
+	// Create the string to which the numbers will be added.
+	string arrayAsString = "";
+
+	// For every item in the array,
+	for (int i = 0; i < arrLength; ++i)
+	{
+		// Convert a number to a string,
+		arrayAsString += to_string(arr[i]);
+		// and if the number is not the last in the array,
+		if (i + 1 < arrLength)
+		{
+			// add a comma.
+			arrayAsString += ", ";
+		}
+	}
+
+	// Return the final array, formatted as a string and separated by commas.
+	return arrayAsString;
+}
 
 // A function for swapping a pair of values by their references.
 // Given the references for each value.
@@ -99,48 +125,182 @@ int binarySearch(int arr[], int len, int search, bool findLast = false)
 	return place;
 }
 
-// A function for formatting an array of integers into a string.
-// Given the array, and its length.
-string formatArray(int arr[], int arrLength)
-{
-	// Create the string to which the numbers will be added.
-	string arrayAsString = "";
-
-	// For every item in the array,
-	for (int i = 0; i < arrLength; ++i)
-	{
-		// Convert a number to a string,
-		arrayAsString += to_string(arr[i]);
-		// and if the number is not the last in the array,
-		if (i + 1 < arrLength)
-		{
-			// add a comma.
-			arrayAsString += ", ";
-		}
-	}
-
-	// Return the final array, formatted as a string and separated by commas.
-	return arrayAsString;
+void clearScreen(int lines = 50) {
+	cout << string(lines, '\n');
 }
 
+// Function for prompting a user to input a yes or no.
+// Takes the prompt to the user, the exception prompt, and returns a boolean.
+bool boolInput( const char* prompt, const char* exception = "Sorry, that's an invalid answer.  Please answer with a yes or no." )
+{
+	string input = "";
 
-// The main loop
+	while (true)
+	{
+		cout << prompt << " ";
+		cin >> input;
+		cout << "\n";
+
+		transform(input.begin(), input.end(), input.begin(),
+			[](unsigned char c) { return static_cast<char>(std::tolower(c)); });
+
+		if (input == "yes" || input == "y" || input == "sure" || input == "ok" || input == "true")
+		{
+			return true;
+		}
+		else if (input == "no" || input == "n" || input == "nope" || input == "false")
+		{
+			return false;
+		}
+
+		cout << exception << "\n\n";
+	}
+	return false;
+}
+
+// Function for prompting user to pass an integer.
+// Takes the prompt to the user, the exception prompt, and returns an integer.
+int intInput( const char* prompt, const char* exception = "Sorry, that's an invalid answer.  Please enter an integer.")
+{
+	int input = 0;
+
+	while (true)
+	{
+		cout << prompt << " ";
+		cin >> input;
+		cout << "\n";
+	
+		if (cin.fail())
+		{
+			cout << exception << "\n\n";
+			continue;
+		}
+		
+		return input;
+
+	}
+
+	return 0;
+
+}
+
+int intArrayInput(int arr[], const char* prompt, unsigned int max = 100, const char* exception = "Sorry, that's an invalid answer.  Please enter a series of integers." )
+{
+	string input = "";
+	stringstream ss;
+	
+	int ints = 0;
+	int buffer = 0;
+
+	bool overflow = false;
+	bool firstLoop = true;
+
+	while (true)
+	{
+		cout << prompt << " ";
+		
+		if (firstLoop)
+		{
+			cin.clear(); cin.ignore(INT_MAX, '\n');
+			firstLoop = false;
+		}
+
+		getline(cin, input);
+
+		if (input.length() >= 256)
+		{
+			cout << "Sorry, please keep your input to 256 characters or under." << "\n\n";
+			continue;
+		}
+
+		ss.str(input);
+
+		cout << "\n";
+		try
+		{
+			while (ss >> buffer)
+			{
+				if (ints == max)
+				{
+					overflow = true;
+					break;
+				}
+				arr[ints] = buffer;
+				ints++;
+			}
+		}
+		catch (const std::exception&)
+		{
+			cout << exception << "\n\n";
+			continue;
+		}
+		if (overflow)
+		{
+			cout << "You included too many integers in your input.  Please keep your number of integers under " << max << "." << "\n\n";
+			overflow = false;
+			continue;
+		}
+		break;
+	}
+	cout << "\n" << formatArray(arr, ints) << ".\n";
+	return ints;
+}
+
+// The main loop (driver function).
 int main()
 {
+	// Align all future text to bottom of the screen for continuity.
+	clearScreen();
 	// A greeting and explanation of the program.
 	cout << "BINARY SEARCH\n";
 	cout << "by Zachary Morritt\n\n";
-
+	
 	cout << "This program is meant to take a chosen amount of numbers as an input, then sort them in either ascending or descending order.\n";
 	cout << "The only accepted inputs will be numbers.  If an incorrect input is entered, you will be ask to re-enter new values.\n";
 	cout << "If you wish, you can also choose a .txt file with numbers on each line, which will also be sorted.\n";
 	cout << "If you have any questions, don't hesitate to ask me for support.\n\n";
 
-	// Ask the user to press enter in order to continue after they've finished reading.
-	cout << "PRESS ANY KEY TO CONTINUE...\n";
-	cin.ignore();
+	// Ask the user to press enter to continue after they've finished reading.
+	cout << "PRESS RETURN TO CONTINUE...\n";
+	cout << flush; cin.ignore(); clearScreen();
+
+	// Ask the user whether or not they'd like to provide their own values.
+	cout << "First off, would you like to insert your own series of numbers?\n";
+	bool manualInput = boolInput("(y/n):", "Please answer with either yes or no.");
+
+	int arr[100] = { 8, 2, 6, 7, 2, 3, 1 };
+	int len = 7;
+
+	// Managing if a user decides to provide their own values.
+	if (manualInput)
+	{
+
+	}
+	// Managing if the user decides to use the stock values.
+	else
+	{
+		string stringArr = formatArray(arr, len);
+
+		cout << "Ok, we'll use the stock array [" << stringArr << "] which is " << len << " characters long.\n";
+	}
+
+	sortArray(arr, len);
+	string sortedStringArr = formatArray(arr, len);
+
+	cout << "Sorted, your array is [" << sortedStringArr << "].\n\n";
+
+	int test[100];
+	int inputLen = intArrayInput(test, "Please insert a list of characters:");
+	cout << formatArray(test, inputLen);
+
+	//intInput("Insert:", "Not right.");
+
 
 	
+	// Ask the user to press enter to exit.
+	cout << flush;
+	cout << "PRESS RETURN TO EXIT...\n";
+	cin.ignore();
 }
 
 /* MANUAL INPUT TESTING CODE - REPLACED BY USER INPUT
